c35d9aa74fb0d5efce0ad210cfe3a60cd309be47
drinkingkazu <kazuhiro@nevis.columbia.edu>
2014-12-10 18:54:03 -0600

    more comments --kazu

--- mac/print_fcl.py
+++ mac/print_fcl.py
@@ -1,10 +1,27 @@
 from ROOT import ubpsql
 from ROOT import TString
 import sys
-i = ubpsql.ConfigReader()
-j = i.RunConfigFromName(sys.argv[1])
 
-a = ubpsql.FhiclMaker()
-a.set(j)
-s=a.pset().dump()
-print s
+if len(sys.argv) < 2:
+    print
+    print '    Provide RunConfig name as 1st argument (for which fcl is dumped)'
+    print
+    sys.exit(1)
+
+reader = ubpsql.ConfigReader()
+
+print
+print '    Attempting fcl dump for a Run-Config:',sys.argv[1]
+print
+if not reader.ExistRunConfig(sys.argv[1]):
+    print
+    print '        Not a registered Run-Config...'
+    print
+    sys.exit(1)
+
+cfg = reader.RunConfigFromName(sys.argv[1])
+
+fcl_maker = ubpsql.FhiclMaker()
+fcl_maker.set(cfg)
+fcl_txt=fcl_maker.pset().dump()
+print fcl_txt
