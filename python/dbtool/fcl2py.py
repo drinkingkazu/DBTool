102477cb855d113c95405b650c6aa1931a82a2d7
Yun-Tse Tsai <yuntse@pas.rochester.edu>
2015-11-30 02:49:26 -0600

    allows a fcl parameter to have a space character within double quotes

--- python/dbtool/fcl2py.py
+++ python/dbtool/fcl2py.py
@@ -14,16 +14,35 @@ def Parse( s ):
     w = t.split()
     # add commas
     tw = []
+    word_holder = 'NONE'
+    append_comma = False
     for iWord, word in enumerate(w):
         if word not in Tokens and word[0] != '"':
-            tw.append('"'+word+'"')
+            if word_holder == 'NONE':
+                tw.append('"'+word+'"')
+                append_comma = True
+            elif word[-1] == '"':
+                word = '%s %s' % ( word_holder, word )
+                tw.append('"\\\"%s\\\""' % word.replace('"',''))
+                word_holder = 'NONE'
+                append_comma = True
+            else:
+                word_holder = '%s %s' % ( word_holder, word )
+                append_comma = False
         elif word[0] == '"':
-            #print '"\"%s\""' % word.replace('"','')
-            tw.append('"\\\"%s\\\""' % word.replace('"',''))
+            if word[-1] == '"':
+                #print '"\"%s\""' % word.replace('"','')
+                tw.append('"\\\"%s\\\""' % word.replace('"',''))
+                append_comma = True
+            else:
+                word_holder = word
+                append_comma = False
+            
         else:
             tw.append(word)
+            append_comma = True
         if word not in [ ':', '{', '[', ',' ]:
-            if iWord+1 < len(w) and not w[iWord+1] in [ ':', '}', ']', ',' ]:
+            if iWord+1 < len(w) and not w[iWord+1] in [ ':', '}', ']', ',' ] and append_comma:
                 tw.append(",")
 
     js = " ".join(tw)
